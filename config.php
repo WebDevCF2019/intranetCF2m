d640cdca4d8bdce08cb3c1a9cfb77a4e1d11bbd6
Amir <amir.a.webdev@gmail.com>
2019-10-18 10:02:41 +0200

    mise à jour

--- config.php
+++ config.php
@@ -1,20 +1,20 @@
 <?php
 // constantes de connexion à la DB
-<<<<<<< HEAD
+
 define("DB_HOST", "localhost");
 define("DB_NAME", "intranetV4");
 define("DB_LOGIN", "root");
 define("DB_PWD", "");
 define("DB_PORT", "3306");
 define("DB_CHARSET", "utf8");
-=======
+
 define("DB_HOST","127.0.0.1");
 define("DB_NAME","intranetv4");
 define("DB_LOGIN","root");
 define("DB_PWD","");
 define("DB_PORT","3306");
 define("DB_CHARSET","utf8");
->>>>>>> 8633bf7c6492332271fa7ae5db255e60326e9cf0
+
 
 // default development mode, change to true for product mode
 define("PRODUCT",false);
@@ -34,10 +34,10 @@ define("IMG_ORIGIN",UPLOAD_RACINE."origin\\");
 // chemin pour les images redimensionnées (avec les proportions)	
 define("IMG_MEDIUM",UPLOAD_RACINE."medium\\");	
 
-<<<<<<< HEAD
+
 // chemin pour les images coupées et redimesionnées (carrées)
 define("IMG_ORIGIN",UPLOAD_RACINE."thumb/");
-=======
+
 // chemin pour les images coupées et redimesionnées (carrées)	
 define("IMG_THUMB",UPLOAD_RACINE."thumb\\");
->>>>>>> 8633bf7c6492332271fa7ae5db255e60326e9cf0
+
